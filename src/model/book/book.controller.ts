e72736b724026fae14690bfb78b8d83cbde61fa3
nhnajmul17 <nhnajmul17@gmail.com>
2023-07-15 19:41:13 +0600

    Get all books Implemented

--- src/model/book/book.controller.ts
+++ src/model/book/book.controller.ts
@@ -1,8 +1,47 @@
-// import { Request, Response } from "express";
-// import { Book } from "./book.model";
+import { Request, Response } from "express";
+import { Book } from "./book.model";
 
-// export const BookController = {
-//   getAllBooks,
-//   getSingleBook,
-//   deleteBook,
+// const CreateBook = async (req: Request, res: Response) => {
+//   try {
+//     const { orderItems, phone } = req.body;
+
+//     // Create the order
+//     const order = await Book.create({ phone });
+
+//     // Create the order items
+//     const orderItemPromises = orderItems.map(async (item: IOrderItem) => {
+//       const { product, quantity } = item;
+//       const orderItem = await OrderItem.create({
+//         orderId: order._id,
+//         product,
+//         quantity,
+//       });
+//     });
+//     await Promise.all(orderItemPromises);
+
+//     res.status(200).json({ message: "Order created successfully" });
+//   } catch (error) {
+//     console.log("Error creating order:", error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while creating the order" });
+//   }
 // };
+
+const getAllBooks = async (req: Request, res: Response) => {
+  try {
+    const books = await Book.find({});
+    await res.json(books);
+  } catch (error) {
+    console.log("Error retrieving Books:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the Books" });
+  }
+};
+
+export const BookController = {
+  getAllBooks,
+  getSingleBook,
+  deleteBook,
+};
--- src/model/book/book.route.ts
+++ src/model/book/book.route.ts
@@ -1,10 +1,10 @@
-// import express from "express";
-// import { BookController } from "./book.controller";
+import express from "express";
+import { BookController } from "./book.controller";
 
-// const router = express.Router();
-// router.get("/book/:id", BookController.getSingleBook);
-// router.delete("/books/:id", BookController.deleteBook);
-// router.get("/books", BookController.getAllBooks);
-// // router.post("/books", BookController.CreateBook);
+const router = express.Router();
+router.get("/book/:id", BookController.getSingleBook);
+router.delete("/books/:id", BookController.deleteBook);
+router.get("/books", BookController.getAllBooks);
+// router.post("/books", BookController.CreateBook);
 
-// export default router;
+export default router;
